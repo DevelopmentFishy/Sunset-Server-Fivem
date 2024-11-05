f4a1620ebe45d605616e8382f532cfa4ddc71998
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 21:30:49 +0100

    update

---
+++ resources/[smartphone]/qs-videos/config/config.js
@@ -0,0 +1,3 @@
+Config = []
+
+Config.Webhook = 'https://discord.com/api/webhooks/1191003001024942180/BBUEUvvieUtPZ59b7WhPsSCow283zz1JHVJzjOovo6WdCQwq_XP46n1gMt-rY6o2sTuV' // Set your own discord Webhook here.

a7028c1aac230833e9758e11571a0845e33038a0
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 22:20:46 +0100

    update

---
+++ resources/Politie_AUTOS/fxmanifest.lua
@@ -0,0 +1,61 @@
+fx_version 'adamant'
+game 'gta5'
+ 
+files {
+    'data/**/*.meta',
+	'els/takel.xml',
+    'els/takelflatbed.xml',
+    'els/takelflatbed2.xml',
+    'els/TT6.xml',
+    'els/1seriep.xml',
+    'els/5seriesp.xml',
+    'els/a45.xml',
+    'els/a45f.xml',
+    'els/FEDM5.xml',
+    'els/BEARCAT2.xml',
+    'els/BMWMOTO.xml',
+    'els/bmwserie3.xml',
+    'els/DSIQ5.xml',
+    'els/DSIVITO.xml',
+    'els/dsu2.xml',
+    'els/dsurange.xml',
+    'els/FEDRANGE.xml',
+    'els/FEDXC90.xml',
+    'els/imprezap.xml',
+    'els/inprezaoud.xml',
+    'els/jaguarfp.xml',
+    'els/jagup.xml',
+    'els/LOKA6.xml',
+    'els/lokmotor.xml',
+    'els/LOKRANGE.xml',
+    'els/LOKXC90.xml',
+    'els/lpcla45.xml',
+    'els/palpha.xml',
+    'els/PORACLE.xml',
+    'els/predator.xml',
+    'els/PSCHAFTER.xml',
+    'els/s3.xml',
+    'els/skodalok.xml',
+    'els/SRTM5.xml',
+    'els/SRTRS6.xml',
+    'els/t6lpa.xml',
+    'els/touran.xml',
+    'els/unmarkedmery.xml',
+    'els/wpv70.xml',
+    'els/Skoda.xml',
+    'els/skoda.xml',
+	'els/audia6.xml',
+	'els/fpmoto3.xml',
+    'els/GD-X7MUG.xml',
+    'els/transporter.xml',
+    'els/22m5sal.xml'
+
+}
+
+is_els 'true'
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'

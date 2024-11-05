beca806d77f31ce3fff7051ab89225e2bf5a8709
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 17:34:46 +0100

    update

---
+++ resources/[Mappen]/diables_scripts/locales/en.lua
@@ -0,0 +1,19 @@
+Locales['en'] = {
+    --Mains
+    ['hands_use']           = "Was je handen",
+    ['hands_progress']      = "Handen aan het wassen",
+    ['hands_notif']         = "Handen aan het wassen",
+
+    --Douches
+    ['showers_use']           = "Neem een douche",
+    ['showers_progress']      = "Aan het douchen",
+    ['showers_notif']         = "Aan het douchen",
+	
+    -- Perf
+    ['TakePerf']           = "Take perfusion",
+	
+    --Bed
+    ['PushBed']           = "Push bed",
+    ['OffLie']           = " Get up",
+    ['OnLie']            = " Lie down"
+}

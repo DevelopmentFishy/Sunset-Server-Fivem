526a7a192e8c30f2dc67f709c269168d325df62f
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 21:53:03 +0100

    update

---
+++ resources/energy_vehclothfastcustoms/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'bodacious'
+game 'gta5'
+
+files {
+    'data/**/**.meta',
+    'data/**/**.meta',
+    'data/**/**.meta',
+    'data/**/**.meta',
+    'data/**/**.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/**/**.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/**.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/**.meta'
+data_file 'CARCOLS_FILE' 'data/**/**.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/**.meta'
+
+escrow_ignore {
+    "stream/**/*.yft"
+} 
+
+
+dependency '/assetpacks'

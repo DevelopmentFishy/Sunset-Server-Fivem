467cf39616b714eea044893def5ac24d9e589b89
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 17:19:55 +0100

    update

---
+++ resources/[kleren]/kleren/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_sunsetkleren.meta',
+  'mp_f_freemode_01_mp_f_sunsetkleren.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_sunsetkleren.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_sunsetkleren.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'

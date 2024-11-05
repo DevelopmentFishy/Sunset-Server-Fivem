9ea9f33b36489bac9993cbb85383c658bc2fa9ac
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 15:33:04 +0100

    update

---
+++ resources/[bmt_cars]/CLA45/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+ 
+files {
+    'data/vehicles.meta',
+    'data/carvariations.meta',
+    'data/handling.meta',
+	'stream/cla45p.xml'
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+
+is_els 'true'
+dependency '/assetpacks'
+dependency '/assetpacks'

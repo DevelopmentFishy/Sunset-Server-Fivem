0352f4cccbf6255b5aa5743c7a684eeb54ae48af
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 17:13:49 +0100

    update

---
+++ resources/[k4mb1]/k4mb1-vinewoodhouse2/client.lua
@@ -0,0 +1,10 @@
+Citizen.CreateThread(function()
+	RequestIpl("k4mb1_hillhouse2_milo_")
+	
+-- House2
+	local interiorid = GetInteriorAtCoords(247.9133, 218.0428, 105.2830)
+
+	RefreshInterior(interiorid)
+	
+end)
+

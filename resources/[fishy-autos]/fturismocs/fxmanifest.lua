29bcdb61bc4094b53c3fe4b13ab984a558873c55
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 16:06:33 +0100

    update

---
+++ resources/[fishy-autos]/fturismocs/fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version 'cerulean'
+
+game { 'gta5' }
+
+description ''
+
+files {
+    'data/**/*.meta'
+}
+ 
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+--data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+-- data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
+-- data_file 'CARCONTENTUNLOCKS_FILE' 'data/**/carcontentunlocks.meta'
+
+-- --client_script 'vehicle_names.lua'
+-- files {
+-- 	'audioconfig/*.dat151.rel',
+-- 	'audioconfig/*.dat54.rel',
+-- 	'sfx/**/*.awc',
+-- 	'sfx/**/*.awc',
+--   }
+
+--   data_file 'AUDIO_GAMEDATA' 'audioconfig/sentinelsg3_game.dat151'
+-- data_file 'AUDIO_SOUNDDATA' 'audioconfig/sentinelsg3_sounds.dat54'
+-- data_file 'AUDIO_WAVEPACK' 'sfx/dlc_uberv10'
+-- data_file 'AUDIO_GAMEDATA' 'audioconfig/sentinelsg3_sounds.dat54'
+-- data_file 'AUDIO_SOUNDDATA' 'audioconfig/sentinelsg3d_game.dat151'
+-- data_file 'AUDIO_WAVEPACK' 'sfx/dlc_usentinelsg3'

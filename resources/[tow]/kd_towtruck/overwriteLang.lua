49eee1d490345f316527d112aa6a1d9db37870fd
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 21:42:29 +0100

    update

---
+++ resources/[tow]/kd_towtruck/overwriteLang.lua
@@ -0,0 +1,25 @@
+-------------
+-- USE THIS FILE TO TRANSLATE THE SCRIPT
+-- LANG LOCATION : shared/lang.lua
+-- Copy ONLY values you want edit, NOT ALL the Lang file
+-------------
+
+
+
+Lang['must_store_hook'] = "Je moet de haak eerst opbergen voor met de truck te rijden"
+Lang['store_hook'] = "Druk %s om de haak op te bergen"
+Lang['take_hook'] = "Druk %s om te haak te nemen"
+Lang['press_attach'] = "Druk %s om %s"
+Lang['unhook'] = "Het voertuig los te maken van de flatbed"
+Lang['hook'] = "Het voertuig vast te maken aan de flatbed"
+Lang['unhooked'] = "Voertuig losgekoppeld"
+Lang['hooked'] = "Voertuig gekoppeld"
+Lang['no_vehicle'] = "Er is geen voertuig op de trailer"
+Lang['winch_hold'] = "Houd %s ingedrukt om de winch op te draaien"
+Lang['unwind_hold'] = "Houd %s ingedrukt om de winch af te draaien"
+Lang['winch_attch'] = "Druk %s om de haak te bevestigen aan de wagen"
+Lang['winch_max'] = "Kabel maximum afgedraaid"
+Lang['winch_drop'] = "Druk %s om de haak te laten vallen"
+Lang['winch_take'] = "Druk %s om te haak te nemen"
+Lang['request_control'] = "Neem controle over het voertuig"
+Lang['winch_already_use'] = "De winch is al in gebruik"

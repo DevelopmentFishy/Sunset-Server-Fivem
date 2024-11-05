7f121a11f0145ba5cae3a04d1a38dc5c208b4e0e
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 15:29:46 +0100

    update

---
+++ resources/[bmt_cars]/BMT-MUGRANGER/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'handling.meta',
+	'stream/mugranger.xml',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+
+is_els 'true'
+

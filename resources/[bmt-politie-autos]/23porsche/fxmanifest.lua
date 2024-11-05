62175d2efc3ebd2abbcf80840fedd9bcd4b1940d
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 15:36:28 +0100

    update

---
+++ resources/[bmt-politie-autos]/23porsche/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+
+game { 'gta5' }
+
+description 'Globbing Template made by PLOK'
+
+files {
+    '*.meta',
+}
+ 
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+-- data_file 'DLCTEXT_FILE' 'data//dlctext.meta'
+-- data_file 'CARCONTENTUNLOCKS_FILE' 'data//carcontentunlocks.meta'

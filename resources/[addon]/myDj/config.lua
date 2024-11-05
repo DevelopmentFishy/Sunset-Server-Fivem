3028ae943b286193dce748b879809dd92ecf0d3f
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 15:00:01 +0100

    update

---
+++ resources/[addon]/myDj/config.lua
@@ -0,0 +1,32 @@
+Config = {}
+Translation = {}
+
+Translation = {
+    ['de'] = {
+        ['DJ_interact'] = 'Drücke ~g~E~s~, um auf das DJ Pult zuzugreifen',
+        ['title_does_not_exist'] = '~r~Dieser Titel existiert nicht!',
+    },
+
+    ['en'] = {
+        ['DJ_interact'] = 'Press ~g~E~s~, to access the DJ desk',
+        ['title_does_not_exist'] = '~r~This title does not exist!',
+    }
+}
+
+Config.Locale = 'en'
+
+Config.useESX = false -- can not be disabled without changing the callbacks
+Config.enableCommand = false
+
+Config.enableMarker = true -- purple marker at the DJ stations
+
+Config.DJPositions = {
+    {
+        name = 'bahama',
+        pos = vector3(-1391.35, -604.97, 31.52),
+        requiredJob = nil, -- example: requiredJob = "dj" 
+        range = 25.0, 
+        volume = 1.0 --[[ do not touch the volume! --]]
+    }
+
+}
---
+++ resources/[addon]/okokBanking/locales/en.lua
@@ -0,0 +1,221 @@
+Locales['en'] = {
+
+    -- PIN RELATED LOCALES
+
+    ['no_pin'] = {
+        title = 'BANKING',
+        text = 'Ga eerst naar een bank om een ​​pincode in te stellen',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['pin_changed'] = {
+        title = 'BANKING',
+        text = 'Pincode succesvol gewijzigd in ${s1}',
+        time = 5000,
+        type = 'success'
+    },
+
+    ['pin_no_money'] = {
+        title = 'BANKING',
+        text = 'Je hebt ${s1}€ om uw pincode te wijzigen',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['pin_digits'] = {
+        title = 'BANKING',
+        text = 'Uw pincode moet 4 cijfers lang zijn',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['pin_only_numbers'] = {
+        title = 'BANKING',
+        text = 'Je kunt alleen cijfers gebruiken',
+        time = 5000,
+        type = 'error'
+    },
+
+
+    -- IBAN RELATED LOCALES
+
+    ['iban_not_exist'] = {
+        title = 'BANKING',
+        text = 'Dit IBAN bestaat niet',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['iban_in_use'] = {
+        title = 'BANKING',
+        text = 'Deze IBAN is al in gebruik',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['iban_only_numbers'] = {
+        title = 'BANKING',
+        text = 'In uw IBAN kunt u alleen cijfers gebruiken',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['iban_changed'] = {
+        title = 'BANKING',
+        text = 'IBAN succesvol gewijzigd naar ${s1}',
+        time = 5000,
+        type = 'success'
+    },
+
+    ['iban_no_money'] = {
+        title = 'BANKING',
+        text = 'U heeft ${s1}€ nodig om uw IBAN te wijzigen',
+        time = 5000,
+        type = 'error'
+    },
+
+
+    -- WITHDRAWN / DEPOSITED / TRANSFERRED / RECEIVED
+
+    ['deposited'] = {
+        title = 'BANKING',
+        text = 'Je hebt gestort ${s1}€',
+        time = 5000,
+        type = 'success'
+    },
+
+    ['withdrawn'] = {
+        title = 'BANKING',
+        text = 'Je hebt een afhaling van ${s1}€ gedaan',
+        time = 5000,
+        type = 'success'
+    },
+
+    ['received_from'] = {
+        title = 'BANKING',
+        text = 'Je hebt ${s1}€ Ontvangen van ${s2}',
+        time = 5000,
+        type = 'success'
+    },
+
+    ['transferred_to'] = {
+        title = 'BANKING',
+        text = 'Je hebt overschrijving van ${s1}€ naar ${s2}',
+        time = 5000,
+        type = 'success'
+    },
+
+    ['deposited_to'] = {
+        title = 'BANKING',
+        text = 'Je storte ${s1}€ naar ${s2}',
+        time = 5000,
+        type = 'success'
+    },
+
+    ['someone_withdrawing'] = {
+        title = 'BANKING',
+        text = 'Er is al iemand aan het afhalen',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['you_have_withdrawn'] = {
+        title = 'BANKING',
+        text = 'Je hebt een afhaling van ${s1}€ naar ${s2} gedaan',
+        time = 5000,
+        type = 'success'
+    },
+
+
+    -- GENERAL LOCALES
+
+    ['no_creditcard'] = {
+        title = 'BANKING',
+        text = 'Zonder creditcard heeft u geen toegang tot de geldautomaat',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['invalid_amount'] = {
+        title = 'BANKING',
+        text = 'ongeldige hoeveelheid',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['invalid_input'] = {
+        title = 'BANKING',
+        text = 'Ongeldige invoer',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['no_money_pocket'] = {
+        title = 'BANKING',
+        text = 'Je hebt dit bedrag niet bij je',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['no_money_bank'] = {
+        title = 'BANKING',
+        text = 'Je hebt dit bedrag niet op je bank staan',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['not_send_yourself'] = {
+        title = 'BANKING',
+        text = 'Je kan geen geld naar jezelf sturen',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['society_no_money'] = {
+        title = 'BANKING',
+        text = 'Jullie samenleving heeft niet zoveel geld op de bank',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['not_use_bank'] = {
+        title = 'BANKING',
+        text = 'U kunt de bank momenteel niet gebruiken',
+        time = 5000,
+        type = 'error'
+    },
+
+    ['bought_cc'] = {
+        title = 'BANKING',
+        text = 'Je hebt er een creditcard voor gekocht ${s1}€',
+        time = 5000,
+        type = 'success'
+    },
+
+
+    -- TEXTUI LOCALES
+
+    ['open_banking'] = {
+        text = '[E] toegang bank',
+        color = 'darkblue',
+        side = 'left'
+    },
+
+    ['open_atm'] = {
+        text = '[E] Access ATM',
+        color = 'darkblue',
+        side = 'left'
+    },
+
+
+    -- QB-TARGET LOCALES
+
+    ['open_banking_target'] = {
+        text = 'Toegang bank',
+    },
+
+    ['open_atm_target'] = {
+        text = 'Toegang geldautomaat',
+    },
+}

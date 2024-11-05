a5ea93a772ce4ebe09de37a1e4cb30e878bcab54
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 15:45:59 +0100

    update

---
+++ resources/[botsautos]/bumpercar/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author "Tiwabs#6666"
+version '1.0.0'
+contact 'https://discord.gg/NvrTRdh'
+
+this_is_a_map "yes"
+
+files {
+    'data/vehicles.meta',
+    'data/carvariations.meta',
+    'data/carcols.meta',
+    'data/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+
+
+dependency '/assetpacks'

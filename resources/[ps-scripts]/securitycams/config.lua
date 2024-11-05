c66beb5d0739408d9f91e670d51fce21bac53d79
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 21:07:14 +0100

    update

---
+++ resources/[ps-scripts]/securitycams/config.lua
@@ -0,0 +1,40 @@
+SecurityCamConfig = {}
+
+SecurityCamConfig.DebugMode = false
+SecurityCamConfig.HideRadar = true
+
+SecurityCamConfig.Locations = {
+    {
+        camBox = {label = "CCTV Weed", x = 1044.49, y = -3198.1, z = -38.16},
+        cameras = {
+            {label = "Camera buiten", x = 44.68, y = 6360.8, z = 43.85, r = {x = -30.0, y = 0.0, z = 233.11}, canRotate = false},
+            {label = "Camera inkom", x = 78.78, y = 6290.99, z = 44.46, r = {x = -30.0, y = 0.0, z = 74.0}, canRotate = false},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "CCTV Coke", x = 1087.15, y = -3198.27, z = -38.99},
+        cameras = {
+            {label = "Camera inkom", x = 752.12, y = -3192.22, z = 10.86, r = {x = -30.0, y = 0.0, z = 220.69}, canRotate = false},
+            {label = "Camera buiten", x = 776.65, y = -3234.53, z = 21.57, r = {x = -30.0, y = 0.0, z = 8.0}, canRotate = false},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "CCTV Meth", x = 1002.68, y = -3194.89, z = -38.99},
+        cameras = {
+            {label = "Camera inkom", x = 2568.16, y = 4640.03, z = 44.9, r = {x = -30.0, y = 0.0, z = 329.26}, canRotate = false},
+            {label = "Camera buiten", x = 2530.74, y = 4673.23, z = 46.09, r = {x = -30.0, y = 0.0, z = 287.72}, canRotate = false},
+        },
+        allowedModels = {}
+    },
+    {
+        camBox = {label = "CCTV White", x = -145.57, y = 892.6, z = 233.46},
+        cameras = {
+            {label = "Camera Poort", x = -137.76, y = 896.91, z = 240.89, r = {x = -25.0, y = 0.0, z = 284.41}, canRotate = false},
+            {label = "Camera buiten 1", x = -170.61, y = 961.74, z = 252.36, r = {x = -30.0, y = 0.0, z = 199.31}, canRotate = false},
+            {label = "Camera buiten 2", x = -163.8, y = 867.69, z = 247.7, r = {x = -30.0, y = 0.0, z = 4.66}, canRotate = true},
+        },
+        allowedModels = {}
+    }
+}

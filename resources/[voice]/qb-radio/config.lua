5b3c8affc6042c537b4c4d12e7877b2445dc90f6
DevelopmentFishy <jarondebruyne2024@gmail.com>
2024-11-05 21:46:56 +0100

    update

---
+++ resources/[voice]/qb-radio/config.lua
@@ -0,0 +1,56 @@
+Config = {}
+
+Config.RestrictedChannels = {
+    [1] = {
+        police = true,
+        ambulance = true,
+        mechanic = true
+    },
+    [2] = {
+        police = true,
+        ambulance = true,
+        mechanic = true
+    },
+    [3] = {
+        police = true,
+        ambulance = true,
+        mechanic = true
+    },
+    [4] = {
+        police = true,
+        ambulance = true,
+        mechanic = true
+    },
+    [5] = {
+        police = true,
+        ambulance = true,
+        mechanic = true
+    },
+    [6] = {
+        police = true,
+        ambulance = true,
+        mechanic = true
+    },
+    [7] = {
+        police = true,
+        ambulance = true,
+        mechanic = true
+    },
+    [8] = {
+        police = true,
+        ambulance = true,
+        mechanic = true
+    },
+    [9] = {
+        police = true,
+        ambulance = true,
+        mechanic = true
+    },
+    [10] = {
+        police = true,
+        ambulance = true,
+        mechanic = true
+    }
+}
+
+Config.MaxFrequency = 500
